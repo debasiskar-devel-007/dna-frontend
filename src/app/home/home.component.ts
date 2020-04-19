2de8bc57ef80c8ac33965085342455f958a2176c
amitavainfluxiq <amitava.involutiontech@gmail.com>
2020-04-20 01:25:30 +0530

    amitava inner page html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MetaService } from '@ngx-meta/core';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { window.scrollTo(500, 0); }
+  constructor(public meta: MetaService) { window.scrollTo(500, 0); 
+  
+    this.meta.setTitle('DNA Of Success - The DNA Master Course');
+    // this.meta.update({ name: 'description', content: 'Dynamic Hello Angular Lovers description!' });
+    this.meta.setTag('og:description', 'Jack Zufelt’s The DNA Of Success Master Course to help you identify and overcome barriers using the Success Attitude Formula to achieve success at the high levels you always desired.');
+    this.meta.setTag('twitter:description', 'Jack Zufelt’s The DNA Of Success Master Course to help you identify and overcome barriers using the Success Attitude Formula to achieve success at the high levels you always desired.');
+
+    this.meta.setTag('og:keyword', 'Personal Development Course, Success Attitude Formula, DNA of Success');
+    this.meta.setTag('twitter:keyword', 'Personal Development Course, Success Attitude Formula, DNA of Success');
+
+    this.meta.setTag('og:title', 'DNA Of Success - The DNA Master Course');
+    this.meta.setTag('twitter:title', 'DNA Of Success - The DNA Master Course');
+    this.meta.setTag('og:type', 'website');
+    this.meta.setTag('og:url','https://www.dnamastercourse.com/');
+      this.meta.setTag('og:image', '../../assets/images/logometa.jpg');
+  }
 
   ngOnInit() {
   }
