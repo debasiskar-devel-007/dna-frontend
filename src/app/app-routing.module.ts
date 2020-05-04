af64ec438fa2430ed9828363ff0ff48b33a4db46
amitavainfluxiq <amitava.involutiontech@gmail.com>
2020-05-04 20:11:46 +0530

    amitava landing page html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { LoginComponent } from './login/login.component';
 import { ProductComponent } from './product/product.component';
 import { ContactComponent } from './contact/contact.component';
 import { MentorsignupComponent } from './mentorsignup/mentorsignup.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent}, 
@@ -29,7 +30,7 @@ const routes: Routes = [
   {path:'product', component: ProductComponent}, 
   {path:'contact', component: ContactComponent}, 
   {path:'mentorsignup', component: MentorsignupComponent}, 
-  
+  {path:'landingpage', component: LandingpageComponent}, 
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import { LoginComponent } from './login/login.component';
 import { ProductComponent } from './product/product.component';
 import { ContactComponent } from './contact/contact.component';
 import { MentorsignupComponent } from './mentorsignup/mentorsignup.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
 export function metaFactory(): MetaLoader {
   return new MetaStaticLoader({
@@ -69,6 +70,7 @@ export function metaFactory(): MetaLoader {
     ProductComponent,
     ContactComponent,
     MentorsignupComponent,
+    LandingpageComponent,
   ],
   imports: [
     DemoMaterialModule,
