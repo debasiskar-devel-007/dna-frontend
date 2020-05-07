59a43a2644a08552b230bfc2ce097880cff83135
souravghatak-007 <sourav.influxiq@gmail.com>
2020-05-07 17:18:14 +0530

    push with

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -20,97 +20,92 @@ export class ContactComponent implements OnInit {
   formfieldrefresh: boolean = true;
   updatetable: boolean = true;
   formfieldrefreshdata: any = null;
- 
   //public formdata: any;
 
   
   public formdata: any;
 
-  
-
 
   constructor(public _apiService: ApiService, public ActivatedRoute: ActivatedRoute) {
 
-
-
-
     this._apiService.getclientip().subscribe((res: any) => {
       // console.log(res);
-       this.ip=res.ip;
-       console.log(this.ip)
+        this.ip=res.ip;
+        console.log(this.ip)
+        this.formdata = {
+          successmessage: "Added Successfully !!",
+          redirectpath: "/contact",
+          submittext:"Submit",
+          // canceltext:"Cancel Now",
+          // resettext:"reset This",
+          submitactive:true, //optional, default true
+          apiUrl: this._apiService.api_url,
+          endpoint: 'api/contactus',  //change endpoint
+          jwttoken: this._apiService.jwtToken,
+      
+          fields: [
+            {
+              heading: "",
+              label: " Name",
+              name: "name",
+              value: '',
+              type: "text",
+              validations: [
+                { rule: 'required' },
+                { rule: 'maxLength', value: 10 },
+                { rule: 'minLength', value: 2 }
+              ],
+      
+            },
+      
+            {
+              label: "Email",
+              name: "email",
+              type: 'email',
+              hint: "abc@gmail.com",
+              validations: [
+                { rule: 'required', message: "Email field Needs to be required" },
+                { rule: 'pattern', value: this.emailregex, message: "Must be a valid Email" }]
+            },
+            {
+              label: "Phone",
+              name: "phone",
+              type: 'number',
+              hint: '',
+              validations: [
+                { rule: 'required' }
+      
+              ]
+            },
+            {
+              heading: "",
+              label: "Message",
+              name: "message",
+              value: '',
+              type: "textarea",
+              validations: [
+                { rule: 'required' },
+              ]
+            },
+            {
+              label:"ip",
+              name:"ip",
+              type:'hidden',
+              value:this.ip
+          }
+      
+      
+          ]
+        };
+      
      })
 
-     this.formdata = {
-      successmessage: "Added Successfully !!",
-      redirectpath: "/contact",
-      submittext:"Submit",
-      // canceltext:"Cancel Now",
-      // resettext:"reset This",
-      submitactive:true, //optional, default true
-      apiUrl: this._apiService.api_url,
-      endpoint: '/api/contactus',  //change endpoint
-      jwttoken: this._apiService.jwtToken,
-  
-      fields: [
-        {
-          heading: "",
-          label: " Name",
-          name: "name",
-          value: '',
-          type: "text",
-          validations: [
-            { rule: 'required' },
-            { rule: 'maxLength', value: 10 },
-            { rule: 'minLength', value: 2 }
-          ],
-  
-        },
-  
-        {
-          label: "Email",
-          name: "email",
-          type: 'email',
-          hint: "abc@gmail.com",
-          validations: [
-            { rule: 'required', message: "Email field Needs to be required" },
-            { rule: 'pattern', value: this.emailregex, message: "Must be a valid Email" }]
-        },
-        {
-          label: "Phone",
-          name: "phone",
-          type: 'number',
-          hint: '',
-          validations: [
-            { rule: 'required' }
-  
-          ]
-        },
-        {
-          heading: "",
-          label: "Message",
-          name: "message",
-          value: '',
-          type: "textarea",
-          validations: [
-            { rule: 'required' },
-          ]
-        },
-        {
-          label:"ip",
-          name:"ip",
-          type:'hidden',
-          value:this.ip
-      }
-  
-  
-      ]
-    };
   }
 
 
 
   ngOnInit() {
-    
+
 
   }
 
