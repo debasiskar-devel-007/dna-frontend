717c49893475f9cf5cf51dc99de71b560464c454
mukherjee-priyanka <62089013+mukherjee-priyanka@users.noreply.github.com>
2020-06-18 13:28:34 +0530

    dnafontened

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -171,7 +171,7 @@ const routes: Routes = [
   },
 
   // {path: '', redirectTo: 'home', pathMatch: 'full'},
-  // {path: '**', component: HomeComponent},
+   {path: '**', component: HomeComponent},
 
   { path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) }
 ];
