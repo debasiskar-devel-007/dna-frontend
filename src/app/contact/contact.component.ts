07c22f7d12165ef996f93545229e6d59210b59ef
Arnab-sarkar123 <arnabsarkar.influxiq@gmail.com>
2020-06-18 16:38:21 +0530

    Landing page up to date

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -53,9 +53,9 @@ export class ContactComponent implements OnInit {
               value: '',
               type: "text",
               validations: [
-                { rule: 'required' },
-                { rule: 'maxLength', value: 10 },
-                { rule: 'minLength', value: 2 }
+                { rule: 'required', message: "Enter Your Name" },
+                //{ rule: 'maxLength', value: 10 },
+                //{ rule: 'minLength', value: 2 }
               ],
       
             },
@@ -67,7 +67,7 @@ export class ContactComponent implements OnInit {
               type: 'email',
               //hint: "abc@gmail.com",
               validations: [
-                { rule: 'required', message: "Email field Needs to be required" },
+                { rule: 'required', message: "Enter Your Email Id" },
                 { rule: 'pattern', value: this.emailregex, message: "Must be a valid Email" }]
             },
             {
@@ -77,7 +77,7 @@ export class ContactComponent implements OnInit {
               type: 'text',
               hint: '',
               validations: [
-                { rule: 'required' }
+                { rule: 'required' , message: "Enter Your Contact Number" }
       
               ]
             },
@@ -88,7 +88,7 @@ export class ContactComponent implements OnInit {
               value: '',
               type: "textarea",
               validations: [
-                { rule: 'required' },
+                { rule: 'required' ,message: "Enter Your Message" },
               ]
             },
             {
